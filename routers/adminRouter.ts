ba6e34330f5a206b6d24eaf0ec5f1aba09ff354a
bhatn <nagapooja.bhat@bahag.com>
2024-12-05 23:31:26 +0100

    added admin Role

---
+++ routers/adminRouter.ts
@@ -0,0 +1,10 @@
+import { Router } from 'express';
+import { checkIfAdmin, getAllUsers, getUserById, deleteUser } from '../controllers/adminController';
+import { authenticateJWT } from '../middlewares/jwtMiddleware';
+
+const adminRouter = Router();
+adminRouter.get('/admin/users', authenticateJWT, checkIfAdmin, getAllUsers);
+adminRouter.get('/admin/users/:id', authenticateJWT, checkIfAdmin, getUserById);
+adminRouter.delete('/admin/users/:id', authenticateJWT, checkIfAdmin, deleteUser);
+
+export default adminRouter;
